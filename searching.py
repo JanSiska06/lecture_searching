92bdbcfeceb4de1996b773eb2776e1f63bf083aa
Jan Šiška <274753@vutbr.cz>
2026-04-13 14:39:23 +0200

    only linear done

--- searching.py
+++ searching.py
@@ -1,5 +1,9 @@
+from dataclasses import field
+from itertools import count
 from pathlib import Path
 import json
+
+from generators import ordered_sequence
 
 
 def read_data(file_name, field):
@@ -22,9 +26,48 @@ def read_data(file_name, field):
     
     file_path = cwd_path / file_name
 
+    with open("sequential.json", "r") as file:
+        data = json.load(file)
+    return data[field]
+
+def linear_search(sequence, number):
+    count = 0
+    position = []
+    no_count = 0
+    for i,num in enumerate(sequence):
+        if number == num:
+            count += 1
+            position.append(i)
+        else:
+            no_count += 1
+
+    if no_count < len(sequence):
+        return {
+        'positions': position,
+        'count': count
+    }
+    else:
+        return None
+
+def binary_search(sequence, number):
+    mid = (max + min)/2
 
 def main():
-    pass
+    sequen_data = read_data("sequential.json", 'unordered_numbers')
+    print(sequen_data)
+
+    number = 0
+    linear = linear_search(sequen_data, number)
+    print(linear)
+
+
+    if field() == 'unordered_numbers' or field() == 'ordered_numbers':
+        return list
+    elif field() == 'dna_sequence':
+        return str
+    else:
+        return None
+
 
 
 if __name__ == "__main__":
